068c19c24a581b8e3003d246b0ddaf02d277182d
unknown <t1rajo01@students.oamk.fi>
2022-03-29 17:33:41 +0300

    graafinen suunnittelu aloitettu

--- bankautomat/mainwindow.cpp
+++ bankautomat/mainwindow.cpp
@@ -1,15 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "pinni.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setStyleSheet("background-color: blue;");
+    pPinni = new Pinni;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_avaapin_clicked()
+{
+    pPinni->exec();
 }
 
