9e3d238e6511c7d891130a373c6a013047af29b0
unknown <t1rajo01@students.oamk.fi>
2022-04-15 19:57:54 +0300

    käyttöliittymä ja restapii

--- api/app.js
+++ api/app.js
@@ -20,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/login',loginRouter);
 
 //app.use(authenticateToken);
